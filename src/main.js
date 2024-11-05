3d27d1da5a7620640a865209e552322aa9af1b5b
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-11-05 06:45:20 +0000

    Class Target .hour , .minute, .seconds, .tick

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// == Class Target  
+var hoursContainer = document.querySelector('.hours')
+var minutesContainer = document.querySelector('.minutes')
+var secondsContainer = document.querySelector('.seconds')
+var tickElements = Array.from(document.querySelectorAll('.tick'))
